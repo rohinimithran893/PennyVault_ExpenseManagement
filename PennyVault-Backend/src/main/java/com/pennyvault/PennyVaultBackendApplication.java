dbe334fa9644b8c619e1f874fd424cd03925c223
Rohini <rohinivaishu893@gmail.com>
2026-08-11 18:17:28 +0530

    Initial skeleton commit

---
+++ PennyVault-Backend/src/main/java/com/pennyvault/PennyVaultBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pennyvault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PennyVaultBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PennyVaultBackendApplication.class, args);
+	}
+
+}
---
+++ PennyVault-Backend/src/test/java/com/pennyvault/PennyVaultBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pennyvault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PennyVaultBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
